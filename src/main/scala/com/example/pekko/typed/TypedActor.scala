04c56e64d23094e9d9e0cbbd7253f5a6097c83dd
PJ Fanning <pjfanning@users.noreply.github.com>
2025-05-16 19:12:25 +0100

    Update TypedActor.scala

--- src/main/scala/com/example/pekko/typed/TypedActor.scala
+++ src/main/scala/com/example/pekko/typed/TypedActor.scala
@@ -3,6 +3,9 @@ package com.example.pekko.typed
 import org.apache.pekko.actor.typed.scaladsl.AskPattern.Askable
 import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Behavior}
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.util.Timeout
+
+import scala.concurrent.duration.DurationInt
 
 object TypedActor {
   final case class Greet(whom: String, replyTo: ActorRef[Greeted])
@@ -15,10 +18,9 @@ object TypedActor {
   }
 
   val system: ActorSystem[Greet] = ActorSystem(TypedActor(), "greet")
-  system.ask[Greeted](Greet("World", _)) { replyTo =>
-    Greeted("World", replyTo)
-  }(system.scheduler, system.executionContext).onComplete {
+  val fut = system.ask[Greeted](Greet("World", _))(Timeout(10.seconds), system.scheduler)
+  fut.onComplete {
     case scala.util.Success(Greeted(whom, _)) => system.log.info("Received greeting from {}", whom)
     case scala.util.Failure(exception)        => system.log.error("Failed to greet: {}", exception.getMessage)
-  }
+  }(system.executionContext)
 }
